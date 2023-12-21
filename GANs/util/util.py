10ef6bf74d247b4f64332212757e793fa8e05c9d
Raheem Hashmani <RKHashmani@gmail.com>
2023-12-20 20:08:33 -0800

    Create tensor2npy Function

--- GANs/util/util.py
+++ GANs/util/util.py
@@ -61,6 +61,34 @@ def tensor2im(input_image, imtype=np.uint8):
         image_numpy = input_image
     return image_numpy.astype(imtype)
 
+def tensor2npy(input_image, label, opt):
+    """"Converts a Tensor array into a numpy image array.
+
+    Parameters:
+        input_image (tensor) --  the input image tensor array
+        imtype (type)        --  the desired type of the converted numpy array
+    """
+    if not isinstance(input_image, np.ndarray):
+        if isinstance(input_image, torch.Tensor):  # get the data from a variable
+            image_tensor = input_image.data
+        else:
+            return input_image
+        image_numpy = image_tensor[0].clamp(-1.0, 1.0).cpu().float().numpy()  # convert it into a numpy array
+
+        if label == "real_A":
+            if opt.standardize:  # De-standardize if previously standardized
+                image_numpy = (image_numpy * opt.A_scaled_std) + opt.A_scaled_mean
+            if opt.min_max_scale:  # De-scale if previously scaled
+                image_numpy = (image_numpy * (opt.Amax - opt.Amin)) + opt.Amin
+        else:
+            if opt.standardize:  # De-standardize if previously standardized
+                image_numpy = (image_numpy * opt.B_scaled_std) + opt.B_scaled_mean
+            if opt.min_max_scale:  # De-scale if previously scaled
+                image_numpy = (image_numpy * (opt.Bmax - opt.Bmin)) + opt.Bmin
+    else:  # if it is a numpy array, do nothing
+        image_numpy = input_image
+    return image_numpy
+
 
 def diagnose_network(net, name='network'):
     """Calculate and print the mean of average absolute(gradients)
